200a7dd23f08036199a27a21205672bc48c86f67
Exzotic5485 <61587328+Exzotic5485@users.noreply.github.com>
2024-09-20 11:18:31 +0100

    fix cors issues

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,10 +1,16 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
+import { cors } from "hono/cors";
 import { powersRoute } from "./routes/powers";
 
 const app = new Hono();
 
 app.use(logger());
+app.use(
+    cors({
+        origin: ["http://localhost:5173"],
+    })
+);
 
 const apiRoutes = app.basePath("/api").route("/powers", powersRoute);
 
