a73617a025a6543483d0d257f9e09a70f33fb699
Exzotic5485 <61587328+Exzotic5485@users.noreply.github.com>
2024-09-24 10:00:12 +0100

    update datbase seed.ts

--- apps/api/src/db/seed.ts
+++ apps/api/src/db/seed.ts
@@ -1,8 +1,13 @@
 import { client, db } from ".";
-import { powers } from "./schema";
+import {
+    type PowerWithConfigurableFields,
+    configurableFields,
+    powers,
+} from "./schema";
 
-async function main() {
-    await db.insert(powers).values({
+const POWERS: PowerWithConfigurableFields[] = [
+    {
+        id: 0,
         name: "Test Power",
         description: "A description of the test power.",
         summary: "A short summary...",
@@ -23,7 +28,37 @@ async function main() {
                 },
             ],
         },
-    });
+        configurableFields: [
+            {
+                powerId: 0,
+                id: 0,
+                name: "Command",
+                description:
+                    "Modify the command that is ran when you press primary key.",
+                fieldPath: "entity_action.command",
+            },
+        ],
+    },
+];
+
+async function main() {
+    for (const power of POWERS) {
+        const savedPower = await db
+            .insert(powers)
+            .values({
+                ...power,
+                id: undefined,
+            })
+            .returning();
+
+        for (const configurableField of power.configurableFields) {
+            await db.insert(configurableFields).values({
+                ...configurableField,
+                powerId: savedPower[0].id,
+                id: undefined,
+            });
+        }
+    }
 
     await client.end();
 }
