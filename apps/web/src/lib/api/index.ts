a56064754e6928e92e77f38434dac7fcbcc8d471
Exzotic5485 <61587328+Exzotic5485@users.noreply.github.com>
2024-09-24 11:51:03 +0100

    proxy "/api" to api server in vite

--- apps/web/src/lib/api/index.ts
+++ apps/web/src/lib/api/index.ts
@@ -1,4 +1,4 @@
 import type { AppType } from "@repo/api";
 import { hc } from "hono/client";
 
-export const client = hc<AppType>("http://localhost:3000");
+export const client = hc<AppType>("/");
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,13 +1,21 @@
-import path from "node:path"
-import react from "@vitejs/plugin-react"
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
-import { defineConfig } from "vite"
+import path from "node:path";
+import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [react(), TanStackRouterVite()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
-})
+    server: {
+        proxy: {
+            "/api": {
+                target: "http://localhost:3000",
+                changeOrigin: true,
+            },
+        },
+    },
+});
